1e1f55605fd1ed161316f80bfd95b2e3cec290cc
APARBAN <you@example.com>
2026-09-01 13:27:22 +0530

    first commit

---
+++ 25.py
@@ -0,0 +1,6 @@
+#WAP a program to detect double space in a string.
+# detect = input("Enter a str: ")
+# print(f"Double space detected: {'True' if "  " in detect else 'False'}")
+
+text = input("Enter a string: ")
+print(text.find("  "))
