6c309738099152f79dbb731bc41c7b66c4e37e37
APARVAN SHARMA <2024a1r015@mietjammu.in>
2026-09-08 12:21:07 +0530

    Update 50.py

--- 50.py
+++ 50.py
@@ -1,7 +1,12 @@
 #wa p to simulate a digital lock system
 #the lock should be ask the user to enter a 4 dig pin. if the entered pin does not contain exactly 4 dig, the prog should display an error mesg and ask again . if the the enterd pin is correct, the lock should open. otherwise, the prog should ask the user to try again.
+#wa p to simulate a digital lock system
+#the lock should be ask the user to enter a 4 dig pin. if the entered pin does not contain exactly 4 dig, the prog should display an error mesg and ask again . if the the enterd pin is correct, the lock should open. otherwise, the prog should ask the user to try again.
 pin = input("Enter pin: ")
-if len(pin)==4 and pin.digit():
+correct_pin = "1234"
+if len(pin)==4 and pin.isdigit():
     print("PIN is correct")
+elif pin == correct_pin:
+    print("PIN is correct. The lock is open.")
 else:
     print("Error msgg, Ask again")
