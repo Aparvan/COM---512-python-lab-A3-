1fc43e59d2f444483916ccc2abd63f23821b2793
APARBAN <you@example.com>
2026-09-16 11:50:51 +0530

    first commit

---
+++ 68.py
@@ -0,0 +1,14 @@
+'''
+wap to print Floyd's triangle.
+1
+23
+456
+78910
+'''
+n = int(input("enter the number: "))
+count = 0
+for i in range( 1,n+1): 
+    for j in range(1,i+1):
+        count+= 1
+        print(count, end=" ")
+    print()
